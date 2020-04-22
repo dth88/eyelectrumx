44fe294f0445dd9cdc02d0c5b099b911752b9689
dathbezumniy <gdath100500@gmail.com>
2020-04-22 15:34:08 +0700

    staging commit...

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def main():
     electrum_urls = electrum_lib.restore_electrums_from_backup()
     return render_template('index.html', electrum_urls=electrum_urls)
 
-    
+
 @app.route("/atomicdex-mobile")
 def filter_mobile():
     electrum_urls = electrum_lib.restore_electrums_from_backup()
@@ -26,6 +26,7 @@ def filter_mobile():
 def explorers():
     explorers_urls = electrum_lib.restore_explorers_from_backup()
     return render_template('explorers.html', explorers_urls=explorers_urls)
+
 
 #scheduler = BackgroundScheduler()
 #scheduler.add_job(func=mine_one_block_first_node, trigger="interval", seconds=2)
