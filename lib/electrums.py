c7809aab55af1282894e5c5485c81c4c8764727f
dathbezumniy <gdath100500@gmail.com>
2020-06-18 01:43:37 +0300

    manually updated tickers...

--- lib/electrums.py
+++ lib/electrums.py
@@ -1,28 +1,36 @@
-all_tickers = ['AXE', 'AYWA', 'BCH', 'BET', 'BOTS', 'BTC',
-               'BTCH', 'BTG', 'CCL', 'CHIPS', 'COQUI', 'CRYPTO',
-               'D', 'DASH', 'DEX', 'DGB', 'DOGE', 'ECA', 'EMC2',
-               'FJC', 'FTC', 'GAME', 'GIN', 'GRS', 'HODL', 'HODLC',
-               'HUSH', 'ILN', 'JUMBLR', 'K64', 'KMD', 'KOIN', 'KV',
-               'LABS', 'LTC', 'LUMBER', 'MCL', 'MGW', 'MNX', 'MONA',
-               'MORTY', 'MSHARK', 'MUE', 'NAV', 'NINJA', 'OOT',
-               'PAC', 'PANGEA', 'PGT', 'PTX', 'QTUM', 'REVS', 'RFOX',
-               'RICK', 'RVN', 'SMART', 'SUPERNET', 'THC', 'UFO', 'VRSC',
-               'VTC', 'WLC', 'XBC', 'XZC', 'ZEC', 'ZILLA', 'ETH']
+all_tickers = ['AXE',      'BCH',     'BET',    'BOTS',     'BTC',      'BTCH',  
+               'BTG',      'CCL',     'CHIPS',  'COQUI',    'CRYPTO',   'D',        
+               'DASH',     'DEX',     'DGB',    'DOGE',     'ECA',      'EMC2',     
+               'FTC',      'GAME',    'GIN',    'GRS',      'HODL',     'HODLC',   
+               'HUSH',     'ILN',     'JUMBLR', 'KMD',      'KOIN',     'LABS',   
+               'LTC',      'LUMBER',  'MCL',    'MGW',      'MONA',     'MORTY',   
+               'MUE',      'MSHARK',  'NAV',    'NINJA',    'NMC',      'OOT',
+               'PANGEA',   'PGT',     'PTX',    'QTUM',     'REVS',     'RFOX',  
+               'RICK',     'RVN',     'SMART',  'SUPERNET', 'THC',      'UFO',
+               'VOTE2020', 'VRSC',    'VTC',    'WLC',      'XBC',      'XZC',
+               'ZEC',      'ZER',     'ZILLA',  'ETH']
 
+#'AYWA', "AWC", "BAT", "DAI", 'FJC', "VOTE", 'K64', 'KV', 'MNX', "PAX", 'PAC', "TUSD" "BUSD"
 #official electrums/parity-node repo links
 link = "https://raw.githubusercontent.com/KomodoPlatform/coins/master/electrums/"
 eth_link = "https://raw.githubusercontent.com/KomodoPlatform/coins/master/ethereum/"
 
 
-#filter
-atomic_dex_mobile = ["KMD", "BTC", "AWC", "AXE", "BUSD", "BAT",
-                     "BCH", "ZILLA", "DAI", "DASH", "DEX", "DGB",
-                     "DOGE", "ECA", "ETH", "FTC", "HUSH", "LABS",
-                     "LTC", "MORTY", "NAV", "PAX", "QTUM", "RVN",
-                     "RFOX", "RICK", "SAI", "SUPERNET", "TUSD",
-                     "USDC", "OOT", "VRSC", "VOTE", "ZEC"]
+#filters
+atomic_dex_mobile = ['KMD',  'BTC', 'BCH',  'DASH', 'DEX', 'DGB',
+                     'LTC',  'ETH',  'USDC',   'MORTY',  'RICK',  
+                     'HUSH', 'LABS',  'QTUM', 'RVN',  'ZEC', 
+                     'SUPERNET', 'VRSC', 'AXE', 'ZILLA', 'DOGE',
+                     'ECA', 'FTC', 'NAV', 'RFOX', 'OOT', 'VOTE2020']
+                     
 
+atomic_dex_pro =    ["KMD",  "BTC",  "BCH",  "DASH", "DEX",  "DGB",
+                     "LTC", "ETH", "USDC", "MORTY", "RICK", "HUSH",
+                     "LABS",  "QTUM",  "RVN",  "ZEC",  "SUPERNET",  
+                     "VRSC", "BET", "BOTS", "CRYPTO", "ILN" "JUMBLR", 
+                     "MCL", "MGW", "PANGEA", "REVS", "CHIPS"]
 
+                             
 #electrum calls
 electrum_version_call = {
     "jsonrpc" : "2.0",
