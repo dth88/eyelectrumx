c5977af045fec9764c0b1d18878be190a0cfcb44
dathbezumniy <gdath100500@gmail.com>
2020-06-21 21:12:58 +0300

    fix jsonerror on electrum ping job

--- app.py
+++ app.py
@@ -178,7 +178,9 @@ def gather_and_backup_electrums():
     updated_urls = {}
     
     with open('data/backup_electrums.json') as electrum_urls:
-        electrumz = json.load(electrum_urls)
+        electrum_urls = electrum_urls.read()
+        electrum_urls = electrum_urls[:-1]
+        electrumz = json.loads(electrum_urls)
         updated_urls = electrum_lib.call_electrums_and_update_status(electrumz, electrums.electrum_version_call, electrums.eth_call)
 
     with open('data/backup_electrums.json', 'w') as f:
